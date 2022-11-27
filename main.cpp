482ea86102be5e7bce6e1db9ec733d68a77fc437
remperazKevin <remperazkevin@gmail.com>
2022-11-28 02:30:20 +0800

    1.3.0-alpha
    Added credit card expiry check.
    Added customer name check.
    Added customer phone check.
    Added random receipt number generator.
    Changed to separate credit card expiry.
    Replaced range-based for-loops.
    Removed unnecessary index variables.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 // TODO Code Review
-// TODO Create return codes
 // TODO Code Cleanup
 
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
@@ -11,14 +11,11 @@ using namespace std;
 // Global Variable
 char userInput;
 
-// Ignore complex data-flow analysis
+// Ignore CLion complex data-flow analysis
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCDFAInspection"
 // Truck Selection
 struct truckSelection {
-    int indexCategory = 0;
-    int indexType = 0;
-
     int truckCategory = 0;
     int truckType = 0;
 
@@ -32,7 +29,6 @@ struct truckSelection {
 struct orderInvoice {
     int rentDays = 0;
 
-    int indexBranch = 0;
     int branchLocation = 0;
     string selectedBranch;
 };
@@ -43,12 +39,12 @@ struct customerInformation {
     string customerEmail;
 };
 struct paymentInfo {
-    int indexPayment = 0;
     int paymentMode = 0;
     string selectedPayment;
 
     string cardNumber;
-    string cardExpiry;
+    int cardExpiryMonth = 0;
+    int cardExpiryYear = 0;
     string cardCVV;
 };
 
@@ -141,7 +137,7 @@ int main() {
             {
                     0,
                     20500,
-                    3000,
+                    30000,
                     25500,
                     0,
                     0,
@@ -187,12 +183,13 @@ int main() {
 
     cout << "[Truck Selection - Truck Category]" << endl;
     cout << "Please select a truck category" << endl;
-    for (auto & category : truckList) {
-        cout << "[" << ++selection.indexCategory << "] " << category[0] << endl;
+    int category;
+    for (category = 0; category < 5; category++) {
+        cout << "[" << category + 1 << "] " << truckList[category][0] << endl;
     }
     cout << "Enter truck category number: ";
     cin >> selection.truckCategory;
-    if (selection.truckCategory > selection.indexCategory) {
+    if (selection.truckCategory > category) {
         cout << "Please only select from the given truck category!";
         return 0;
     }
@@ -203,18 +200,19 @@ int main() {
 
     cout << "[Truck Selection - Truck Type]" << endl;
     cout << "Please select a truck type" << endl;
-    for (auto & type : truckList[selection.truckCategory]) {
-        if (type == truckList[selection.truckCategory][0] || type == "\u200b") {
-            // Skip first element of array
+    int type;
+    for (type = 0; type < 7; type++) {
+        if (type == 0 || truckList[selection.truckCategory][type] == "\u200b") {
+            // Skip element of array
             continue;
         }
         else {
-            cout << "[" << ++selection.indexType << "] Php " << truckPrice[selection.truckCategory][selection.indexType] << " - " << type << endl;
+            cout << "[" << type << "] Php " << truckPrice[selection.truckCategory][type] << " - " << truckList[selection.truckCategory][type] << endl;
         }
     }
     cout << "Enter truck type number: ";
     cin >> selection.truckType;
-    if (selection.truckType > selection.indexType) {
+    if (selection.truckType > type) {
         cout << "Please only select from the given truck types!";
         return 0;
     }
@@ -241,12 +239,13 @@ int main() {
 
     cout << "[Billing Section - Branch Location]" << endl;
     cout << "Please select a branch near you" << endl;
-    for (auto & branch : branchLocations) {
-        cout << "[" << ++invoice.indexBranch << "] " << branch << endl;
+    int branches;
+    for (branches = 0; branches < 17; ++branches) {
+        cout << "[" << branches << "] " << branchLocations[branches] << endl;
     }
     cout << "Enter branch number: ";
     cin >> invoice.branchLocation;
-    if (invoice.branchLocation > invoice.indexBranch) {
+    if (invoice.branchLocation > --branches) {
         cout << "Please only select from the given branches!";
         return 0;
     }
@@ -264,19 +263,23 @@ int main() {
     cout << "[Billing Section - Customer Information]" << endl;
     cout << "Enter customer full name: ";
     getline(cin, profile.customerName);
-    // TODO Add name format
+    if (profile.customerName.length() < 2) {
+        cout << "Please input at least two characters!";
+        return 0;
+    }
 
     cout << "Enter customer address: ";
     getline(cin, profile.customerAddress);
-    // TODO Add address format
 
     cout << "Enter customer phone number: ";
     getline(cin, profile.customerPhone);
-    // TODO Add number check
+    if (profile.customerPhone.length() < 11) {
+        cout << "Please input valid phone number!";
+        return 0;
+    }
 
     cout << "Enter customer email address: ";
     getline(cin, profile.customerEmail);
-    // TODO Add email check
 
     cout << endl << "[Customer Information Overview]" << endl;
     cout << "Customer Name:          " << profile.customerName << endl;
@@ -289,12 +292,13 @@ int main() {
 
     cout << "[Billing Section - Mode of Payment]" << endl;
     cout << "Please select a mode of payment (Additional Php 50 fee if non-cash)" << endl;
-    for (auto & mode : paymentModes) {
-        cout << "[" << ++payment.indexPayment << "] " << mode << endl;
+    int mode;
+    for (mode = 0; mode < 4; mode++) {
+        cout << "[" << mode + 1 << "] " << paymentModes[mode] << endl;
     }
     cout << "Enter mode of payment number: ";
     cin >> payment.paymentMode;
-    if (payment.paymentMode > payment.indexPayment) {
+    if (payment.paymentMode > mode) {
         cout << "Please only select from the given mode of payments!";
         return 0;
     }
@@ -312,16 +316,25 @@ int main() {
             cout << "Please print and present the receipt at your chosen branch to process the payment" << endl << endl;
             break;
         default:
-            cout << "Enter card number: ";
+            cout << "Enter card number: (spaces included) ";
             getline(cin, payment.cardNumber);
             if (payment.cardNumber.length() != 19) {
                 cout << "Please input the correct card number!";
                 return 0;
             }
 
-            cout << "Enter card expiry date: (MM/YY) ";
-            getline(cin, payment.cardExpiry);
-            // TODO add card expiry date check
+            cout << "Enter card expiry month: (MM) ";
+            cin >> payment.cardExpiryMonth;
+            if (payment.cardExpiryMonth < 0 || payment.cardExpiryMonth > 12) {
+                cout << "Please input the correct card expiry month!";
+                return 0;
+            }
+            cout << "Enter card expiry year: (YY) ";
+            cin >> payment.cardExpiryYear;
+            if (payment.cardExpiryYear < 22) {
+                cout << "Please input the correct card expiry year!";
+                return 0;
+            }
 
             cout << "Enter card CVV: ";
             cin >> payment.cardCVV;
@@ -332,17 +345,20 @@ int main() {
 
             cout << "[" << payment.selectedPayment << " Payment Overview" << endl;
             cout << "Card Number:      ends in " << payment.cardNumber.substr(15, 4) << endl;
-            cout << "Card Expiry Date: " << payment.cardExpiry << endl;
+            cout << "Card Expiry Date: " << setw(2) << setfill('0') << payment.cardExpiryMonth << "/" << payment.cardExpiryYear << endl;
             cout << "Card CVV:         " << payment.cardCVV << endl << endl;
     }
 
 
     // Receipt
-    // TODO create random receipt number generator
+    const int MIN_RECEIPT_NUMBER = 111111;
+    const int MAX_RECEIPT_NUMBER = 999999;
+    // For now we use the old method of creating a random number in between a range
+    long int receiptNumber  = MIN_RECEIPT_NUMBER + (rand() % (MAX_RECEIPT_NUMBER - MIN_RECEIPT_NUMBER + 1));
 
     double invoiceRentDays  = ((0.1 * sqrt(invoice.rentDays)) / 0.5) * selection.selectedPrice;
     double invoiceInsurance = (40 * (double) selection.selectedPrice) / 100;
-    const double CARD_FEE = (payment.paymentMode > 0) ? 50 : 0;
+    const double CARD_FEE   = (payment.paymentMode > 0) ? 50 : 0;
     double invoicePreTotal  = selection.selectedPrice + invoiceRentDays + invoiceInsurance + CARD_FEE;
     double invoiceTax       = invoicePreTotal * 0.10;
     double invoicePostTotal = invoicePreTotal + invoiceTax;
@@ -350,8 +366,9 @@ int main() {
     // Print receipt (invoice, customer invoice, mode of payment)
     cout << "[Truck Rental System - Official Receipt]" << endl;
     cout << "   Order Invoice" << endl
+         << "       Receipt Number:    " << receiptNumber << endl
          << "       Truck Selection:   " << selection.selectedCategory << " - " << selection.selectedType << " (Php " << selection.selectedPrice << ")" << endl
-         << "       Days to rent:      " << invoice.rentDays << " days (Php " << invoiceRentDays << ")"<< endl
+         << "       Days to rent:      " << invoice.rentDays << " day/s (Php " << invoiceRentDays << ")"<< endl
          << "       Branch Location:   " << invoice.selectedBranch << endl << endl;
     cout << "   Customer Info" << endl
          << "       Name:              " << profile.customerName << endl
@@ -362,7 +379,7 @@ int main() {
         cout << "   Mode of Payment" << endl
              << "       Payment via:       " << payment.selectedPayment << endl
              << "       Card Number:       ends in " << payment.cardNumber.substr(15, 4) << endl
-             << "       Card Expiry Date:  " << payment.cardExpiry << endl << endl;
+             << "       Card Expiry Date:  " << setw(2) << setfill('0') << payment.cardExpiryMonth << "/" << payment.cardExpiryYear << endl << endl;
         cout << "   Misc." << endl
              << "       Insurance:         Php " << invoiceInsurance << endl
              << "       Card Fee:          Php " << CARD_FEE << endl << endl;
